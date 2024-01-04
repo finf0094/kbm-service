40b4c48c74182816b4e65f468dc5f434178f14ea
finf0094 <finf0094@gmail.com>
2024-01-04 06:10:15 +0500

    check allowed

--- backend/src/main/java/kz/qbm/app/AppApplication.java
+++ backend/src/main/java/kz/qbm/app/AppApplication.java
@@ -2,12 +2,16 @@ package kz.qbm.app;
 
 import kz.qbm.app.config.StorageProperties;
 import kz.qbm.app.service.storage.StorageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+
+@Slf4j
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 public class AppApplication {
@@ -19,9 +23,17 @@ public class AppApplication {
 	@Bean
 	CommandLineRunner init(StorageService storageService) {
 		return (args) -> {
-			storageService.deleteAll();
 			storageService.init();
 		};
 	}
 
+	@Value("cors.allowed_origins")
+	private String ALLOWED_ORIGINS;
+
+	@Bean
+	CommandLineRunner check() {
+		return (args) -> {
+			log.info("ALLOWED_ORIGINS: " + ALLOWED_ORIGINS);
+		};
+	}
 }
