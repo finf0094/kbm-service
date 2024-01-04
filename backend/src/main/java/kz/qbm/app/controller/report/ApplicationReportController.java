635ecc6f8b31cc14494a7f264257e0c5498449b8
finf0094 <finf0094@gmail.com>
2024-01-04 21:12:30 +0500

    report controller name was changed

--- backend/src/main/java/kz/qbm/app/controller/report/ApplicationReportController.java
+++ backend/src/main/java/kz/qbm/app/controller/report/ApplicationReportController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/position-report")
+@RequestMapping("/api/report")
 @RequiredArgsConstructor
 public class ApplicationReportController {
 
